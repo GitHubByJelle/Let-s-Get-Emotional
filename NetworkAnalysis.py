18eea36b25e7259ef3e7fac759a146d4851b2841
GitHubByJelle <71011269+GitHubByJelle@users.noreply.github.com>
2021-05-17 15:38:02 +0200

    Added one extra network

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -12,17 +12,17 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
 
 
-# trainloader, testloader, _, _ = loaders.make_loader_small(1)
-network_path = 'networks/JNet15_nepoch50_lr0.001_batchsize25_loaderbalanced.pth'
+trainloader, testloader, _, _ = loaders.make_loader_small(1)
+network_path = 'networks/JNet16_nepoch50_lr0.001_batchsize25_loaderbalanced.pth'
 #
 # visualiser.show_convolution_layers(network_path, trainloader)
 #
 # visualiser.visualise_important_area(network_path, trainloader)
 #
 # visualiser.visualise_network(network_path, trainloader)
-
-_, testloader, _, _ = loaders.make_loader(16)
-network = torch.load(network_path, map_location=torch.device('cpu'))
-visualiser.testshow(testloader,network,4,4)
-
-determine_performance(network, testloader)
+#
+# _, testloader, _, _ = loaders.make_loader(16)
+# network = torch.load(network_path, map_location=torch.device('cpu'))
+# visualiser.testshow(testloader,network,4,4)
+#
+# determine_performance(network, testloader)
